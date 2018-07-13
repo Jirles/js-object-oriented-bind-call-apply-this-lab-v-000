9ef7b51758ad75a65b18957085c604c2762bf217
Emily Jirles <emily.jirles@gmail.com>
2018-07-13 15:10:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ function justInvoke(fn) {
   return fn();
 };
 
-
+function setThisWithCall(fn){
+//  calls and returns the function as a callback, and assigns this to be bob while passing an argument to the invoked function
+  return fn.call();
+}
