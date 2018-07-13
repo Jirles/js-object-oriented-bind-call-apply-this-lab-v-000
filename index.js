b03186910cd463f8aa203fbdd842d8df4d33373e
Emily Jirles <emily.jirles@gmail.com>
2018-07-13 15:21:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ function setThisWithCall(fn, obj, arg){
 //  calls and returns the function as a callback, and assigns this to be bob while passing an argument to the invoked function
   return fn.call(obj, arg);
 }
+
+function setThisWithApply(){
+
+};
