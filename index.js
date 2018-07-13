e56fae721588267e7c9b3025be60e0ab79b99011
Emily Jirles <emily.jirles@gmail.com>
2018-07-13 15:20:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ function justInvoke(fn) {
 
 function setThisWithCall(fn, obj, arg){
 //  calls and returns the function as a callback, and assigns this to be bob while passing an argument to the invoked function
-  return fn.call(bob, arg);
+  return fn.call(obj, arg);
 }
